0e41669a936ce39d5794e901fd0e5b8ecacca527
shahin4 <shahinrajr@gmail.com>
2024-04-28 00:47:08 +0600

    adding style

---
+++ frontend_react/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import About from './container/About/About';
+import Footer from './container/Footer/Footer';
+import Header from './container/Header/Header';
+import Skills from './container/Skills/Skills';
+import Testimonial from './container/Testimonial/Testimonial';
+import Work from './container/Work/Work';
+import './App.scss';
+
+
+import  Navbar  from "./components/Navbar/Navbar";
+
+const App = () => (
+  <div className="app">
+   <Navbar /> 
+    <Header />
+    <About />
+    <Work />
+    <Skills />
+    <Testimonial />
+    <Footer />
+  </div>
+);
+
+export default App;
